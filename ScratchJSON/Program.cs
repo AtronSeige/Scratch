2f52d76fc6bd4cc1e7214748680aa3fad4a5d72b
Atron Seige <1794327+AtronSeige@users.noreply.github.com>
2022-03-07 14:22:32 +1300

    ScratchJSON added

---
+++ ScratchJSON/Program.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml;
+
+namespace ScratchJSON {
+	internal class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello World!");
+
+			XmlDocument doc = new();
+
+			doc.LoadXml("<prontoorder xmlns:jsonns=\"http://james.newtonking.com/projects/json\"><customer_reference>11932</customer_reference><account_code>0033014</account_code><warehouse_code>201</warehouse_code><priority_code>5</priority_code><rep_code>FEM</rep_code><territory_code>310</territory_code><date>04-03-2022</date><addresses xmlns:jsonns=\"http://james.newtonking.com/projects/json\" jsonns:Array=\"true\"><address_type>DA</address_type><address1 /><address2>1 queen str</address2><address4>CDB</address4><address5>VIC</address5><address6>AUS</address6><address7>Melbourne</address7><postcode>3000</postcode><phone>123456789</phone></addresses><lines xmlns:jsonns=\"http://james.newtonking.com/projects/json\" jsonns:Array=\"true\"><line_type>SN</line_type><external_id>3011</external_id><item_code>165837BLAXS</item_code><item_price>440.0000</item_price><item_price_tax_code>T</item_price_tax_code><ordered_qty>1</ordered_qty></lines><lines xmlns:jsonns=\"http://james.newtonking.com/projects/json\" jsonns:Array=\"true\"><line_type>SC</line_type><item_price>0.0000</item_price><item_price_tax_code>T</item_price_tax_code><charge_type_flag>0</charge_type_flag><line_description>Standard Delivery</line_description></lines><payments xmlns:jsonns=\"http://james.newtonking.com/projects/json\" jsonns:Array=\"true\"><amount>440.0000</amount><store_id>310</store_id><terminal_no>1</terminal_no><money_type>V</money_type></payments></prontoorder>");
+
+			string json = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.Indented);
+
+			Console.WriteLine(json);
+
+			Console.ReadLine();
+		}
+	}
+}
