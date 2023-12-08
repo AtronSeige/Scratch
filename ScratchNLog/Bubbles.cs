3691fe335d792d0529c38630979efa62d51580b3
Atron Seige <1794327+AtronSeige@users.noreply.github.com>
2023-12-08 17:58:25 +1300

    nlog and target framework changes

---
+++ ScratchNLog/Bubbles.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchNLog {
+	public class Bubbles {
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public DateTime Duration { get; set; }
+		public decimal Radius { get; set; }
+
+		private Logger logger = LogManager.GetCurrentClassLogger();
+
+		public Bubbles() {
+			logger.Info("Bubbles Constructed");
+			logger.Info(this);
+		}
+	}
+}
